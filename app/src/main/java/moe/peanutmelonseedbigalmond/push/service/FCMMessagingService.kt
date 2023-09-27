ee25eb7d4ef360d70569b4edc297ff28a4d0e490
13574 <1357496606@qq.com>
2023-09-27 15:47:44 +0800

    feat: 优化通知
    添加了通知组和渠道组
    优化了 Markdown 通知的显示

--- app/src/main/java/moe/peanutmelonseedbigalmond/push/service/FCMMessagingService.kt
+++ app/src/main/java/moe/peanutmelonseedbigalmond/push/service/FCMMessagingService.kt
@@ -1,12 +1,21 @@
 package moe.peanutmelonseedbigalmond.push.service
 
 import android.graphics.drawable.BitmapDrawable
+import android.text.Spanned
 import android.util.Log
 import coil.imageLoader
 import coil.request.ImageRequest
-import coil.size.Size
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import io.noties.markwon.Markwon
+import io.noties.markwon.ext.strikethrough.StrikethroughPlugin
+import io.noties.markwon.ext.tables.TablePlugin
+import io.noties.markwon.ext.tasklist.TaskListPlugin
+import io.noties.markwon.html.HtmlPlugin
+import io.noties.markwon.image.ImagesPlugin
+import io.noties.markwon.image.coil.CoilImagesPlugin
+import io.noties.markwon.linkify.LinkifyPlugin
+import io.noties.markwon.simple.ext.SimpleExtPlugin
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -18,6 +27,19 @@ import moe.peanutmelonseedbigalmond.push.utils.NotificationUtil
 
 class FCMMessagingService : FirebaseMessagingService(),
     CoroutineScope by CoroutineScope(Dispatchers.Main) {
+    private val markwon by lazy {
+        Markwon.builder(this)
+            .usePlugin(StrikethroughPlugin.create())
+            .usePlugin(TablePlugin.create(this))
+            .usePlugin(TaskListPlugin.create(this))
+            .usePlugin(HtmlPlugin.create())
+            .usePlugin(ImagesPlugin.create())
+            .usePlugin(CoilImagesPlugin.create(this, this.imageLoader))
+            .usePlugin(LinkifyPlugin.create())
+            .usePlugin(SimpleExtPlugin.create())
+            .build()
+    }
+
     override fun onNewToken(token: String) {
         AppConfigurationRepository.fcmPushToken = token
     }
@@ -27,27 +49,14 @@ class FCMMessagingService : FirebaseMessagingService(),
         val messageData = message.data
         val title = messageData["title"] ?: getString(R.string.default_notification_title)
         val messageText = messageData["text"]
-        val messageType = messageData["type"] ?: "text"
+        val messageType = messageData["type"]
 
-        val messageBody = when (messageType) {
-            "text", "image" -> {
-                messageText
-            }
-
-            "markdown" -> {
-                getString(R.string.markdown_norification_brief)
-            }
-
-            else -> {
-                messageText
-            }
-        }
-        if (messageBody != null) {
+        if (messageText != null) {
             if (UserTokenRepository.token.isNotBlank()) { // 登录态
                 when (messageType) {
-                    "text" -> sendTextMessage(title, messageBody)
-                    "image" -> sendImageNotification(title, messageBody)
-                    else -> sendTextMessage(title, messageBody)
+                    "text" -> sendTextNotification(title, messageText)
+                    "image" -> sendImageNotification(title, messageText)
+                    else -> sendMarkdownMessage(title, markwon.toMarkdown(messageText))
                 }
             }
         } else {
@@ -55,8 +64,12 @@ class FCMMessagingService : FirebaseMessagingService(),
         }
     }
 
-    private fun sendTextMessage(title: String, content: String): Int {
-        return NotificationUtil.sendNotification(this, title, content)
+    private fun sendTextNotification(title: String, content: String): Int? {
+        return NotificationUtil.sendTextNotification(title, content)
+    }
+
+    private fun sendMarkdownMessage(title: String, content: Spanned): Int? {
+        return NotificationUtil.sendMarkdownTextNotification(title, content)
     }
 
     private fun sendImageNotification(title: String, imageUrl: String) {
@@ -65,13 +78,12 @@ class FCMMessagingService : FirebaseMessagingService(),
             .build()
         launch {
             val notificationId =
-                sendTextMessage(title, getString(R.string.image_notification_brief))
+                sendTextNotification(title, getString(R.string.image_notification_brief))
             try {
                 val bitmap =
                     withContext(Dispatchers.IO) { imageLoader.execute(imageRequest).drawable as BitmapDrawable? }
                 if (bitmap != null) {
                     NotificationUtil.sendNotificationWithImage(
-                        this@FCMMessagingService,
                         getString(R.string.default_notification_title),
                         bitmap.bitmap,
                         notificationId
--- app/src/main/java/moe/peanutmelonseedbigalmond/push/utils/NotificationUtil.kt
+++ app/src/main/java/moe/peanutmelonseedbigalmond/push/utils/NotificationUtil.kt
@@ -1,81 +1,186 @@
 package moe.peanutmelonseedbigalmond.push.utils
 
+import android.Manifest
+import android.app.Notification
 import android.app.NotificationChannel
+import android.app.NotificationChannelGroup
 import android.app.NotificationManager
-import android.content.Context
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Build
+import android.text.Spanned
+import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.annotation.StringRes
+import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationCompat.BigPictureStyle
+import androidx.core.app.NotificationCompat.BigTextStyle
+import androidx.core.app.NotificationManagerCompat
+import moe.peanutmelonseedbigalmond.push.App
 import moe.peanutmelonseedbigalmond.push.R
 
 object NotificationUtil {
-    private const val notificationChannelId = "high_system"
-    fun setupNotificationChannel(context: Context) {
+    /**
+     * 通知渠道
+     * @property id String
+     * @property notificationChannelName String
+     * @constructor
+     */
+    private sealed class AppNotificationChannel(
+        val id: String,
+        val notificationChannelName: String
+    ) {
+        constructor(id: String, @StringRes notificationChannelName: Int) : this(
+            id,
+            App.context.getString(notificationChannelName)
+        )
+
+        object Markdown : AppNotificationChannel("markdown", R.string.notification_channel_markdown)
+        object Text : AppNotificationChannel("text", R.string.notification_channel_text)
+        object Image : AppNotificationChannel("image", R.string.notification_channel_image)
+    }
+
+    /**
+     * 通知渠道组
+     * @property id String
+     * @property groupName String
+     * @constructor
+     */
+    private sealed class AppNotificationChannelGroup(val id: String, val groupName: String) {
+        constructor(id: String, @StringRes groupName: Int) : this(
+            id,
+            App.context.getString(groupName)
+        )
+
+        object ReceivedMessages : AppNotificationChannelGroup(
+            "received_messages",
+            R.string.notification_channel_group_received_messages
+        )
+    }
+
+    fun setupNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val nm = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            nm.createNotificationChannel(
-                NotificationChannel(
-                    notificationChannelId,
-                    "notification",
-                    NotificationManager.IMPORTANCE_HIGH
-                )
+            val notificationManager = NotificationManagerCompat.from(App.context)
+            notificationManager.createNotificationChannelGroup(AppNotificationChannelGroup.ReceivedMessages)
+            notificationManager.createNotificationChannel(
+                channel = AppNotificationChannel.Image,
+                group = AppNotificationChannelGroup.ReceivedMessages
+            )
+            notificationManager.createNotificationChannel(
+                channel = AppNotificationChannel.Text,
+                group = AppNotificationChannelGroup.ReceivedMessages
+            )
+            notificationManager.createNotificationChannel(
+                channel = AppNotificationChannel.Markdown,
+                group = AppNotificationChannelGroup.ReceivedMessages
             )
         }
     }
 
-    fun sendNotification(
-        context: Context,
+    fun sendTextNotification(
         title: String,
-        content: String,
+        content: CharSequence,
         id: Int? = null,
         time: Long = System.currentTimeMillis()
-    ): Int {
-        val notification = NotificationCompat.Builder(context, notificationChannelId)
+    ): Int? {
+        val notification = AppNotificationChannel.Text.notificationBuilder()
             .setContentTitle(title)
             .setContentText(content)
             .setWhen(time)
             .setSmallIcon(R.drawable.ic_notifications)
             .setLargeIcon(
-                BitmapFactory.decodeResource(
-                    context.resources,
-                    R.drawable.ic_notifications
-                )
+                BitmapFactory.decodeResource(App.context.resources, R.drawable.ic_notifications)
             )
             .build()
-        val nm = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        val notificationId = id ?: time.toInt()
-        nm.notify(notificationId, notification)
-        return notificationId
+        return sendNotification(notification, id ?: time.toInt())
+    }
+
+    fun sendMarkdownTextNotification(
+        title: String,
+        content: Spanned,
+        id: Int? = null,
+        time: Long = System.currentTimeMillis()
+    ): Int? {
+        val notification = AppNotificationChannel.Markdown.notificationBuilder()
+            .setStyle(
+                BigTextStyle()
+                    .bigText(content)
+            )
+            .setContentText(content)
+            .setContentTitle(title)
+            .setWhen(time)
+            .setSmallIcon(R.drawable.ic_notifications)
+            .setLargeIcon(
+                BitmapFactory.decodeResource(App.context.resources, R.drawable.ic_notifications)
+            )
+            .build()
+        return sendNotification(notification, id ?: time.toInt())
     }
 
     fun sendNotificationWithImage(
-        context: Context,
         title: String,
         content: Bitmap,
         id: Int? = null,
         time: Long = System.currentTimeMillis()
-    ): Int {
-        val notification = NotificationCompat.Builder(context, notificationChannelId)
+    ): Int? {
+        val notification = AppNotificationChannel.Image.notificationBuilder()
             .setContentTitle(title)
+            .setLargeIcon(content)
             .setStyle(
                 BigPictureStyle()
                     .bigPicture(content)
+                    .bigLargeIcon(null as Bitmap?)
             )
             .setWhen(time)
-            .setContentText(context.getString(R.string.image_notification_brief))
             .setSmallIcon(R.drawable.ic_notifications)
-            .setLargeIcon(
-                BitmapFactory.decodeResource(
-                    context.resources,
-                    R.drawable.ic_notifications
-                )
-            )
+            .setContentText(App.context.getString(R.string.image_notification_brief))
             .build()
-        val nm = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        val notificationId = id ?: time.toInt()
-        nm.notify(notificationId, notification)
-        return notificationId
+        return sendNotification(notification, id ?: time.toInt())
+    }
+
+    /**
+     * 发送/修改现有通知
+     * @param notification Notification
+     * @param id Int?
+     * @return Int?
+     */
+    private fun sendNotification(notification: Notification, id: Int): Int? {
+        return if (ActivityCompat.checkSelfPermission(
+                App.context,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            Log.w("NotificationUtil", "sendTextNotification: 通知权限被拒绝")
+            null
+        } else {
+            val nm = NotificationManagerCompat.from(App.context)
+            nm.notify(id, notification)
+            id
+        }
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun NotificationManagerCompat.createNotificationChannelGroup(group: AppNotificationChannelGroup) {
+        this.createNotificationChannelGroup(NotificationChannelGroup(group.id, group.groupName))
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun NotificationManagerCompat.createNotificationChannel(
+        channel: AppNotificationChannel,
+        importance: Int = NotificationManager.IMPORTANCE_HIGH,
+        group: AppNotificationChannelGroup? = null
+    ) {
+        val mChannel = NotificationChannel(channel.id, channel.notificationChannelName, importance)
+        if (group != null) {
+            mChannel.group = group.id
+        }
+        this.createNotificationChannel(mChannel)
+    }
+
+    private fun AppNotificationChannel.notificationBuilder(): NotificationCompat.Builder {
+        return NotificationCompat.Builder(App.context, this.id)
     }
 }
