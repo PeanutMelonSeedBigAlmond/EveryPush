63fb7d7fcdae8015a74af9498f3d2020b63b0338
PeanutMelonSeedBigAlmond <PeanutMelonSeedBigAlmond@outlook.com>
2023-11-06 00:09:41 +0800

    fix: 获取预览图顺序错误

--- app/src/main/java/moe/peanutmelonseedbigalmond/push/utils/SpanUtils.kt
+++ app/src/main/java/moe/peanutmelonseedbigalmond/push/utils/SpanUtils.kt
@@ -5,9 +5,16 @@ import androidx.core.text.getSpans
 import io.noties.markwon.image.AsyncDrawableSpan
 
 object SpanUtils {
+    /**
+     * 获取 AsyncDrawableSpan
+     * @param spanned Spanned
+     * @return List<String>
+     * @see <a href="https://www.jianshu.com/p/b188e150b8bb">解决span倒序问题</a>
+     */
     @JvmStatic
     fun findImageUrlFromSpan(spanned: Spanned): List<String> {
-        return spanned.getSpans<AsyncDrawableSpan>(0, spanned.length)
-            .map { it.drawable.destination }
+        val drawSpan = spanned.getSpans<AsyncDrawableSpan>(0, spanned.length)
+        drawSpan.sortBy(spanned::getSpanStart)
+        return drawSpan.map { it.drawable.destination }
     }
 }
